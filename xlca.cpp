0849d0165b9930372655b777229f8655b0f30168
bjyzero <151053262+bjyzero@users.noreply.github.com>
2024-02-22 22:41:15 +0800

    Add files via upload

---
+++ xlca.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(const int &Case)
+{
+    int n;
+    cin >> n;
+
+    //确认和 x 相连的数(向上和向下) 有哪些
+    vector<vector<int>> G(n + 1);
+    for (int i = 1; i < n; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+
+    // 倍增数组，制表
+    vector<array<int, 21>> F(n + 1);
+    vector<int> dep(n + 1);
+
+    function<void(int, int)> dfs = [&](int x, int fax)
+    {
+        F[x][0] = fax;
+        for (int i = 1; i <= 20; ++i)
+        {
+            F[x][i] = F[F[x][i - 1]][i - 1];
+        }
+
+        for (const auto &tox : G[x])
+        {
+            if (tox == fax)
+                continue;
+            dep[tox] = dep[x] + 1;
+            dfs(tox, x);
+        }
+    };
+
+    dfs(1, 0);
+
+    auto glca = [&](int x, int y)
+    {
+        if (dep[x] < dep[y])
+            swap(x, y);
+
+        // 将 x 跳到和 y 相同的深度
+        int d = dep[x] - dep[y];
+        for (int i = 20; i >= 0; --i)
+        {
+            if (d >> i & 1)
+                x = F[x][i];
+        }
+
+        if (x == y)
+            return x;
+
+        // 循环顺序不能变，必须倒序
+        for (int i = 20; i >= 0; --i)
+        {
+            if (F[x][i] != F[y][i])
+            {
+                x = F[x][i];
+                y = F[y][i];
+            }
+        }
+        return F[x][0];
+    };
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << glca(x, y) << '\n';
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int T = 1;
+    for (int i = 1; i <= T; ++i)
+        solve(i);
+    return 0;
+}
